413dc4d2aad7c77606e11670681343908bf5667f
Gabriel Catice <gabrielcatice@Gabriels-MacBook-Air.local>
2017-11-05 12:28:59 -0200

    i dont know waht im doing

---
+++ SocialNetworking/Util/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  SocialNetworking
+//
+//  Created by Gabriel Catice on 11/5/17.
+//  Copyright © 2017 Gabriel Catice. All rights reserved.
+//
+
+import UIKit
+let SHADOW_GRAY: CGFloat = 128.0 / 255.0
---
+++ SocialNetworking/View/FancyField.swift
@@ -0,0 +1,29 @@
+//
+//  FancyField.swift
+//  SocialNetworking
+//
+//  Created by Gabriel Catice on 11/5/17.
+//  Copyright © 2017 Gabriel Catice. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+}
